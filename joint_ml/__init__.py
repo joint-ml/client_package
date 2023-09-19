0c8939d0cf2e85ad27758057d682445e7e79217a
s.nikishonkov <s.nikishonkov@astondevs.ru>
2023-09-19 12:06:10 +0300

    fx init

--- joint_ml/__init__.py
+++ joint_ml/__init__.py
@@ -3,4 +3,4 @@ from _metric import Metric
 from _client_abstract_methods import load_model, get_dataset, train, test
 
 
-__all__ = [Client, Metric, load_model, get_dataset, train, test]
+__all__ = ["Client", "Metric", "load_model", "get_dataset", "train", "test"]
