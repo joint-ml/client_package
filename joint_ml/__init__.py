fb6650de0759127be0e18eee1fde47eaf15bb323
a.shmigider <a.shmigider@astondevs.ru>
2023-09-23 22:41:40 +0400

    add local path for init imports

--- joint_ml/__init__.py
+++ joint_ml/__init__.py
@@ -1,6 +1,6 @@
-from _base_client import Client, save_weights, save_metric, save_output
-from _metric import Metric
-from _client_abstract_methods import load_model, get_dataset, train, test
+from ._base_client import Client, save_weights, save_metric, save_output
+from ._metric import Metric
+from ._client_abstract_methods import load_model, get_dataset, train, test
 
 
 __all__ = [
